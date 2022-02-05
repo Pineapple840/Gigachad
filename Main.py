bfe8f4689968fbddec4710eafd8a295f0fee3b0d
Pineapple840 <Pineapple840@protonmail.com>
2022-02-05 15:53:38 +0000

    help command

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 # bot.py
 #test2
+import datetime
 import random
 import os
 
@@ -10,13 +11,15 @@ from discord.ext import commands
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix='^')
-
+bot = commands.Bot(command_prefix='^',help_command = None)
+client = discord.Client
 GUILD = os.getenv('DISCORD_GUILD')
+
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} is connected to Discord')
-@bot.command(name='ip')
+
+@bot.command()
 async def ip(ctx,member : discord.Member = None):
     if member is None:
         member = ctx.message.author
@@ -25,4 +28,15 @@ async def ip(ctx,member : discord.Member = None):
     response = f'{random.randrange(0,255)}.{random.randrange(0,255)}.{random.randrange(0,255)}.{random.randrange(0,255)}'
     await ctx.send(response)
 
+@bot.command()
+async def help(ctx):
+    await ctx.send("""
+    **ip** - Get a user's ip adress
+    """)
+
+@bot.command()
+async def message(ctx,channel : discord.Channel, text):
+    
+
 bot.run(TOKEN)
+
