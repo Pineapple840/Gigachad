8fa6ad7161fe1bd447577cef86ec6185bd1a1399
Pineapple840 <pineapple840@protonmail.com>
2022-02-10 21:23:38 +0000

    gay sex

--- Main.py
+++ Main.py
@@ -38,23 +38,19 @@ async def help(ctx):
 async def sendmessage(ctx,channel:discord.TextChannel, *text):
     await channel.send(' '.join(text))
 
-@bot.event
-async def on_message(message):
-    channel = bot.get_channel('458778457539870742')
-    if message.server is None and message.author != bot.user:
-        await bot.send_message(channel, message.content)
-    await bot.process_commands(message)
+@bot.command()
+async def gaysex(ctx, member1 : discord.Member = None, member2 : discord.Member = None):
+    if member1 is None:
+        member1 = ctx.message.author.name
+    else:
+        member1 = member1.name
+    if member2 is None:
+        member2 = "Gigachad"
+    else:
+        member2 = member2.name
 
-# This always sends the same message to the same person.  Is that what you want?
-@bot.command(pass_context=True)
-@commands.is_owner()  # The account that owns the bot
-async def dm(ctx):
-    memberID = "ID OF RECIPIENT"
-    person = await bot.get_user_info(memberID)
-    await bot.send_message(person, "WHAT I'D LIKE TO SAY TO THEM")
-    await bot.delete_message(ctx.message)
-
-
+    output = ("Kai is watching TV on his bed, when his boyfriend Alex comes home from the store. The two share a passionate kiss. Kai smirks devilishly and gestures Alex to come closer. Alex aroused quickly stripes naked. Kai does the same. Kai bends over on his knees into position on the bed. Alex quickly shoves his rock hard cock inside of Kai's ass. Alex pounds Kai as hard as he can. Kai's sexy moans make Alex come inside him. After Alex takes his cock out of Kai's ass, Alex pins Kai to the bed and spreads his legs apart while lifting them up. Alex puts his cock slowly in Kai's hole. Alex starts pushing in and pulling out at a steady pace. Kai moans while panting. After a few minutes they both come from pleasure. Kai, eager to get back at Alex, gets on his knees and starts sucking Alex's cock. Alex begins to moan with every mouth stroke. Kai starts to deep throat Alex and fondle his balls. Alex comes uncontrollably into Kai's mouth. Kai swallows it all and kisses the tip of Alex's cock as a tease. After the intense sex, the two took showers together and cuddled each other to sleep.").replace("Kai",member1)
+    output2 = (output).replace("Alex",member2)
+    await ctx.send(output2)
 
 bot.run(TOKEN)
-
